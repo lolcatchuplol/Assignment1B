b749a23d8673ec2515334071cff8379cfb8b8c86
Karlamon <zbv9522@aut.ac.nz>
2015-05-15 09:08:32 +1200

    Create about.php

---
+++ about.php
@@ -0,0 +1,18 @@
+<!--Karl Smith (1390533)-->
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="style.css">
+	<title>About this assignment - Article Searcher</title>
+</head>
+<body>
+    <h1>Article Searcher</h1>
+    <h2>About this assignment</h2>
+    <p>Assignment members:</p>
+    <li>Liang Chen ()</li>
+    <li>Qingyang Guo (1395160)</li>
+    <li>Karl Smith (1390533)</li>
+    <p><a href="index.php">Return to Home Page</a></p>
+</body>
+</html>
