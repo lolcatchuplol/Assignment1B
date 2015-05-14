62498896bb2c86941d476b81e8f401a27dfb4822
Karlamon <zbv9522@aut.ac.nz>
2015-05-15 09:05:25 +1200

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!--Karl Smith (1390533)-->
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="style.css">
+	<title>Status Posting System</title>
+</head>
+<body>
+	<h1>Article Searcher</h1>
+    <h2>Home Page</h2>
+    <p><a href="submitarticleform.php">Submit new article</a><br>
+        <a href="searcharticleform.php">Search article</a><br>
+        <a href="about.php">About this assignment</a></p>
+</body>
+</html>
