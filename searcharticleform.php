f7ef5e86ab3f5e44d6c123a56d6e04992b004f98
Karlamon <zbv9522@aut.ac.nz>
2015-05-15 09:06:46 +1200

    Create searcharticleform.php

---
+++ searcharticleform.php
@@ -0,0 +1,19 @@
+<!--Karl Smith (1390533)-->
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="style.css">
+	<title>Search article - Article Searcher</title>
+</head>
+<body>
+	<h1>Article Searcher</h1>
+    <h2>Search article</h2>
+    <form action="searcharticleprocess.php" method="get">
+        <p align="left"><label><b>Title: </b>
+        <input type="text" name="title" maxlength="100" size="100"</label><br>
+        <input type="submit" value="Search"></p>
+    </form>
+	<p><a href="index.php">Return to Home Page</a></p>
+</body>
+</html>
